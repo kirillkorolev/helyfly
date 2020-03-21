d92687a939e70f1eb4e26d079ce3080a414d2786
Кирилл <kkirillcore@gmail.com>
2020-03-21 19:52:50 +0300

    Update 2020-03-21T16:52:43.211Z

--- source/js/mask.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $("#form-date").mask("00/00/0000");
-  $("#form-phone").mask("+7(000)-000-00-00");
-  $("#form-card-number").mask("0000.0000.0000.0000");
-  $("#form-card-validity").mask("00/00");
-});
